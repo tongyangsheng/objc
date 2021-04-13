93e9186db37ed0a180f07ada3216aaa660925124
tongyangsheng <dream.master.tong@gmail.com>
2021-04-13 10:22:36 +0800

    compiled successfully

---
+++ objc-test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  objc-test
+//
+//  Created by 童阳升 on 2021/4/13.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
